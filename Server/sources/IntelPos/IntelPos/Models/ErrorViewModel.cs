bed9445dda4db8ec5692f389ec31db716a1d48de
Игорь Чиёсов <i.chiesov@medved.studio>
2020-12-07 23:48:43 +0700

    Server initial commit.

---
+++ Server/sources/IntelPos/IntelPos/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace IntelPos.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
