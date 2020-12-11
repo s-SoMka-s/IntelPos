04333072a590f0ac62c11d97a7ef4ce504f027c0
Игорь Чиёсов <i.chiesov@medved.studio>
2020-12-11 21:18:12 +0700

    master Server add Card model.

---
+++ Server/sources/IntelPos/IntelPos/ApplicationContext.cs
@@ -0,0 +1,23 @@
+﻿using IntelPos.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace IntelPos
+{
+    public class ApplicationContext : DbContext
+    {
+        public DbSet<Card> Cards { get; set; }
+
+        private const string host = "intelpos.postgres.database.azure.com";
+        private const string port = "5432";
+        private const string database = "IntelPos-dev";
+        private const string userName = "postgre@intelpos";
+        private const string password = "IntelPos2020";
+
+        public ApplicationContext() { }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder builder)
+        {
+            builder.UseNpgsql($"Host={host};Port={port};Database={database};Username={userName};Password={password};SslMode=Require");
+        }
+    }
+}
---
+++ Server/sources/IntelPos/IntelPos/Migrations/20201211140742_Initial.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
+
+namespace IntelPos.Migrations
+{
+    public partial class Initial : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Cards",
+                columns: table => new
+                {
+                    Id = table.Column<long>(type: "bigint", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    Term = table.Column<string>(type: "text", nullable: true),
+                    Definition = table.Column<string>(type: "text", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Cards", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Cards");
+        }
+    }
+}
